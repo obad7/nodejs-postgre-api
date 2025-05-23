dc90c11339f6caf0344b64bed3547dba57a49f87
Abdelrahman <apoeljzzar@gmail.com>
2025-05-23 13:10:20 +0300

    Hashing Password

--- src/Services/userService.js
+++ src/Services/userService.js
@@ -1,10 +1,14 @@
 import pool from "../DB/connection.js";
+import { hash } from "../Utils/hashing.js";
 
 export const createUser = async (name, email, password) => {
+
+    const hashedPassword = hash({ plainText: password });
+
     const result = await pool.query(
         `INSERT INTO users (name, email, password)
          VALUES ($1, $2, $3) RETURNING *`,
-        [name, email, password]
+        [name, email, hashedPassword]
     );
     return result.rows[0];
 };

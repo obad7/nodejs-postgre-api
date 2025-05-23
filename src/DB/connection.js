313b6fa4845ff69717d80852d90f4c6724d875aa
Abdelrahman <apoeljzzar@gmail.com>
2025-05-23 12:22:35 +0300

    Update DB Connection

--- src/DB/connection.js
+++ src/DB/connection.js
@@ -1,11 +1,11 @@
 import { Pool } from "pg";
 
 const pool = new Pool({
-    user: "postgres",
-    host: "localhost",
-    database: "nodejs-postgre-api",
-    password: "Obad5775011",
-    port: 5432,
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
 });
 
 pool.on('connect', () => {
